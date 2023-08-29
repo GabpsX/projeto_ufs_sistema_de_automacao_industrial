fddb71e316153ffd3a4c3e60eabedc87af47ba88
GABRIEL P.S <80731506+GabpsX@users.noreply.github.com>
2023-08-29 15:01:03 -0300

    update

--- docs/static/js/dados.js
+++ docs/static/js/dados.js
@@ -1,17 +1,22 @@
 async function downloadImageUsingFetch(url) {
   try {
     let url_download;
+    let url_type;
     switch(url){
       case "pfd":
         url_download = "https://raw.githubusercontent.com/GabpsX/projeto_ufs_sistema_de_automacao_industrial/main/docs/static/build%20offline/PFDdownload.png";
+        url_type = 'pfd.jpg';
         break
       case "pid":
         url_download = "https://raw.githubusercontent.com/GabpsX/projeto_ufs_sistema_de_automacao_industrial/main/docs/static/build%20offline/PIDdownload.png";
+        url_type = 'pid.jpg';
         break
       case "movel":
         url_download = "";
+        url_type = 'SistemaAutprodapApp.apk';
         break
       case "simul":
+        url_type = 'projeto3D.glb';
         url_download = "https://github.com/GabpsX/projeto_ufs_sistema_de_automacao_industrial/raw/main/docs/model3D/projetcompleto.glb";
       default:
         break
@@ -22,7 +27,7 @@ async function downloadImageUsingFetch(url) {
     // Criação do link temporário
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = 'image.jpg'; // Nome do arquivo que será baixado
+    link.download = url_type; 
     document.body.appendChild(link);
     link.click();
 
@@ -48,3 +53,6 @@ bt_downalods.forEach((bt_target) => {
 });
 
 
+function show_popup() {
+  
+}
